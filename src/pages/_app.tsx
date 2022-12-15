d4e3ed5e9b5f083508d71292c8d52b88b5c1390b
Thiago Kachinsky <thiagokachidev@gmail.com>
2022-12-15 20:27:18 -0300

    Create project structure

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,22 @@
-import type { AppProps } from 'next/app';
+import { AppProps } from 'next/app';
+import Head from 'next/head';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+import GlobalStyles from 'styles/global';
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>TKstore</title>
+        <meta
+          name="description"
+          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+export default App;
